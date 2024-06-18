7344b5cd3c0c8261b70bddae83fee7b60ec7ac5f
Seu nome <Seu Email>
2024-06-18 13:47:05 -0300

    vá para o limbo

--- backend/src/main/java/com/challenge/interfaces/repositories/UserJPARepository.java
+++ backend/src/main/java/com/challenge/interfaces/repositories/UserJPARepository.java
@@ -1,9 +1,11 @@
 package com.challenge.interfaces.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.challenge.models.User;
 
 public interface UserJPARepository extends JpaRepository<User, Long> {
-    
+    Optional<User> findByEmail(String email);
 }
--- backend/src/main/java/com/challenge/models/User.java
+++ backend/src/main/java/com/challenge/models/User.java
@@ -1,5 +1,11 @@
 package com.challenge.models;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +15,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,4 +28,40 @@ public class User {
 
     @Column(name = "password")
     private String password;
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
